1c824bc43848f27467cff8aa4ea5df396863eb27
Martim Gancho <martim.af.gancho@gmail.com>
2025-12-04 19:58:29 +0000

    Add files via upload

---
+++ jump and catch )/main/life.go
@@ -0,0 +1,8 @@
+components {
+  id: "life"
+  component: "/main/life.gui"
+  position {
+    x: -0.6587209
+    y: 0.44554263
+  }
+}
---
+++ jump and catch )/main/text_item.go
@@ -0,0 +1,4 @@
+components {
+  id: "item"
+  component: "/main/item.gui"
+}
